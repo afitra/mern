dcff15cdc9b78a9b2bf0f010716263b1bb53d87f
afitra <loeby45@gmail.com>
2020-07-16 16:00:56 +0700

    otw Finish Checkout Page

--- src/elements/Form/index.js
+++ src/elements/Form/index.js
@@ -1,3 +1,5 @@
 export { default as InputDate } from "./InputDate/InputDate.js"
 export { default as InputNumber } from "./InputNumber/InputNumber.js"
+export { default as InputText } from "./InputText/InputText.js"
+export { default as InputFile } from "./InputFile/InputFile.js"
 export { default as Button } from "../Button/Button.js"
